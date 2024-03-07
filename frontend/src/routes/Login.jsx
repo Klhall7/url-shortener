53acdf4ddff380dffbedb86508ceb868a5011d80
Kyra Hall <kyravitch98@gmail.com>
2024-03-07 11:52:26 -0500

    updated action for new url source and access token

--- frontend/src/routes/Login.jsx
+++ frontend/src/routes/Login.jsx
@@ -1,29 +1,46 @@
-import { Form, redirect } from 'react-router-dom';
+import { Form } from 'react-router-dom';
 
-export async function action({ request }) {
-    const userData = await request.formData();
-    const email = userData.get('email');
-    const password = userData.get('password');
-    const data = { email, password};
+export const action = async ({ request }) => {
+    const formData = await request.formData();
+    const email = formData.get('email');
+    const password = formData.get('password');
+    const loginData = { email, password};
 
-    const url = "http://localhost:8000/login";
-    const loginResponse = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-    }).then((response) => response.json())
+    try {
+        const url = `${import.meta.env.VITE_SOURCE_URL}`;
+        const loginResponse = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(loginData)
+            });
 
-    console.log("login response", loginResponse) //debug check
+            console.log("login response", loginResponse) //debug check
 
-    if (loginResponse.detail === "Invalid User Credentials") {
-        window.alert(loginResponse.detail);
-        return null;
-    } else {
-        window.alert(loginResponse.detail);
-        return redirect("/urls");
+            const statusCode = loginResponse.status;
+            const data = await loginResponse.json();
+
+            const { access_token } = data;
+
+            localStorage.clear();
+            localStorage.setItem('access_token', access_token);
+            return statusCode === 200 ? true : false;
+
+    } catch (error) {
+        console.error('ERROR:', error)
+        return false;
     }
+
+    
+
+    // if (loginResponse.detail === "Invalid User Credentials") {
+    //     window.alert(loginResponse.detail);
+    //     return null;
+    // } else {
+    //     window.alert(loginResponse.detail);
+    //     return redirect("/urls");
+    // }
 
 }
 
